87920ffa9e10c9d1bbb6ed15590bef6e9bdf0ea6
Emanuele A. <161335121+numberzeroo@users.noreply.github.com>
2025-05-27 16:28:58 +0200

    fix: https request

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
 import fs from 'fs';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
@@ -8,6 +8,14 @@ export default defineConfig({
       key: fs.readFileSync(path.resolve(__dirname, 'ssl/key.pem')),
       cert: fs.readFileSync(path.resolve(__dirname, 'ssl/cert.pem')),
     },
+    host: true,
     port: 5173,
+    proxy: {
+      '/generate': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 });
